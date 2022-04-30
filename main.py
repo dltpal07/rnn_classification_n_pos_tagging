77389de9dbb87513d5ce266d6a9e7b541b873314
dltpal07 <dltpal07@gmail.com>
2022-04-30 18:55:49 +0900

    Add sources

--- main.py
+++ main.py
@@ -1,16 +1,385 @@
-# This is a sample Python script.
+import pandas as pd
+import torch.nn as nn
+from nltk.tokenize import word_tokenize
+import json
+import torch
+import model
+import argparse
+import torch.optim as optim
+import os
+import csv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu-num', default=3, type=int)
+parser.add_argument('--task', default='classification', type=str, choices=['classification', 'pos'])
+parser.add_argument('--epochs', default=150, type=int)
+parser.add_argument('--do-train', action='store_true', default=False)
+args = parser.parse_args()
+device = torch.device(f'cuda:{args.gpu_num}')
+
+criterion = nn.CrossEntropyLoss()
+isDebug = True
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def tokenization(sents):
+    tokens = []
+    for sent in sents:
+        token = word_tokenize(sent)
+        tokens.append(token)
+    return tokens
 
 
-# Press the green button in the gutter to run the script.
+def pad_sequence(tokens, max_length, pre_padding=True):
+    new_tokens = []
+    pad_start_index = []
+    for token in tokens:
+        # pre-sequence truncation
+        if len(token) > max_length:
+            new_token = token[:max_length]
+            pad_start_index.append(max_length)
+
+        elif len(token) < max_length:
+            addition = max_length - len(token)
+            # pre-padding
+            if pre_padding:
+                new_token = ['[PAD]'] * addition + token
+                pad_start_index.append(0)
+            # post-padding
+            else:
+                new_token = token + ['[PAD]'] * addition
+                pad_start_index.append(len(token))
+        else:
+            new_token = token
+            pad_start_index.append(len(token))
+        new_tokens.append(new_token)
+    return new_tokens, pad_start_index
+
+
+def word2vec(tokens, word_dict, word_embedding_matrix):
+    word_embs = []
+    for token in tokens:
+        word_emb = []
+        for t in token:
+            if t not in word_dict.keys():
+                word_emb.append(word_embedding_matrix[word_dict['[UNK]']])
+            else:
+                word_emb.append(word_embedding_matrix[word_dict[t]])
+        word_emb = torch.stack(word_emb, dim=0)
+        word_embs.append(word_emb)
+    word_embs = torch.stack(word_embs, dim=0)
+    return word_embs
+
+
+def flip_input(tokens, max_length):
+    new_tokens = []
+    for token in tokens:
+        new_token = []
+        if len(token) > max_length:
+            token = token[:max_length]
+        for t_idx in range(len(token)-1, -1, -1):
+            new_token.append(token[t_idx])
+        new_tokens.append(new_token)
+    return new_tokens
+
+
+def output2csv(pred_y, file_name='classification_class.pred.csv'):
+    with open(file_name, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['id', 'label'])
+        for i, p in enumerate(pred_y):
+            y_id = str(i)
+            # if len(y_id) < 3:
+            #     y_id = '0' * (3 - len(y_id)) + y_id
+            writer.writerow(['S' + y_id, p[0].item()])
+    print('file saved.')
+
+
+# train classification
+def classification_train(train_x, train_y, epoch):
+    tr_loss = 0.
+    correct = 0
+    net.train()
+
+    optimizer = optim.Adam(net.parameters(), lr=1e-4, weight_decay=1e-4)
+    batch_size = 256
+    iteration = (len(train_x) // batch_size) + 1
+    cur_i = 0
+    for i in range(1, iteration + 1):
+        if cur_i >= len(train_x):
+            break
+        if i < iteration:
+            data, targets = train_x[cur_i:cur_i + batch_size].to(device), train_y[cur_i:cur_i + batch_size].to(device)
+            cur_i += batch_size
+        if i == iteration:
+            data, targets = train_x[cur_i:].to(device), train_y[cur_i:].to(device)
+            cur_i = len(train_x)
+        optimizer.zero_grad()
+        output = net(data)
+        loss = criterion(output, targets)
+        tr_loss += loss.item()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=1.0)
+        optimizer.step()
+        pred = output.argmax(dim=1, keepdim=True)
+        correct += pred.eq(targets.view_as(pred)).sum().item()
+
+        print("\r[epoch {:3d}/{:3d}] loss: {:.6f}".format(epoch, args.epochs, loss), end=' ')
+
+    tr_loss /= iteration
+    tr_acc = correct / len(train_x)
+    return tr_loss, tr_acc
+
+
+# test classification
+def classification_test(test_x, targets=None):
+    net.eval()
+    correct = 0.
+    ts_loss = 0.
+    with torch.no_grad():
+        data = test_x.to(device)
+        output = net(data)
+
+        pred = output.argmax(dim=1, keepdim=True)
+        if targets != None:
+            targets = targets.to(device)
+            ts_loss = criterion(output, targets)
+            correct += pred.eq(targets.view_as(pred)).sum().item()
+            ts_acc = correct / len(test_x)
+            return ts_loss, ts_acc
+        else:
+            return pred
+
+
+# train pos-tagging
+def pos_train(train_x, reverse_train_x, pad_start_index, max_length, train_y, epoch):
+    tr_loss = 0.
+    correct = 0
+    all_samples = 0
+    net.train()
+
+    optimizer = optim.Adam(net.parameters(), lr=1e-4, weight_decay=1e-4, betas=(0.9, 0.999))
+    batch_size = 256
+    iteration = (len(train_x) // batch_size) + 1
+    cur_i = 0
+    for i in range(1, iteration + 1):
+        if cur_i >= len(train_x):
+            break
+        if i < iteration:
+            data, reverse_data, targets, pad_indexes = train_x[cur_i:cur_i + batch_size].to(device), reverse_train_x[cur_i:cur_i + batch_size].to(device),\
+                                          train_y[cur_i:cur_i + batch_size].to(device), pad_start_index[cur_i:cur_i + batch_size]
+            cur_i += batch_size
+        if i == iteration:
+            data, reverse_data, targets, pad_indexes = train_x[cur_i:].to(device), reverse_train_x[cur_i:].to(device),\
+                                                       train_y[cur_i:].to(device), pad_start_index[cur_i:]
+            cur_i = len(train_x)
+        optimizer.zero_grad()
+        output = net(data, reverse_data, pad_indexes, max_length)
+        targets = targets.view(-1)
+        all_samples += len(targets)
+        # set mask
+        mask = torch.where(targets == 0, False, True).unsqueeze(1).expand(-1, 18)
+        output *= mask
+        loss = criterion(output, targets)
+        tr_loss += loss.item()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=1.0)
+        optimizer.step()
+        pred = output.argmax(dim=1, keepdim=True)
+        correct += pred.eq(targets.view_as(pred)).sum().item()
+
+        print("\r[epoch {:3d}/{:3d}] loss: {:.6f}".format(epoch, args.epochs, loss), end=' ')
+
+    tr_loss /= iteration
+    tr_acc = correct / all_samples
+    return tr_loss, tr_acc
+
+
+# test pos-tagging
+def pos_test(test_x, reverse_test_x, pad_start_index, max_length, targets=None):
+    net.eval()
+    correct = 0.
+    ts_loss = 0.
+
+    with torch.no_grad():
+        if targets != None:
+            data = test_x.to(device)
+            reverse_data = reverse_test_x.to(device)
+            output = net(data, reverse_data, pad_start_index, max_length)
+            pred = output.argmax(dim=1, keepdim=True)
+            targets = targets.to(device)
+            targets = targets.view(-1)
+            ts_loss = criterion(output, targets)
+            correct += pred.eq(targets.view_as(pred)).sum().item()
+            ts_acc = correct / len(targets)
+            return ts_loss, ts_acc
+        else:
+            outputs = None
+            for i in range(len(test_x)):
+                data = test_x[i].to(device)
+                data = data.unsqueeze(0)
+                reverse_data = reverse_test_x[i].to(device)
+                reverse_data = reverse_data.unsqueeze(0)
+                output = net(data, reverse_data, [pad_start_index[i]], max_length)
+                output = output[:pad_start_index[i]]
+                if outputs == None:
+                    outputs = output
+                else:
+                    outputs = torch.cat((outputs, output), dim=0)
+            pred = outputs.argmax(dim=1, keepdim=True)
+            return pred
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(args.task)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # classification
+    if args.task == 'classification':
+        # load _data
+        train_data = pd.read_csv('./dataset/classification/train_set.csv')
+        test_data = pd.read_csv('./dataset/classification/test_set.csv')
+        tr_sents, tr_labels = train_data['sentence'], train_data['label']
+        ts_sents = test_data['sentence']
+
+        # tokenization
+        tr_tokens = tokenization(tr_sents)
+        ts_tokens = tokenization(ts_sents)
+        if isDebug: print(f'tr_sents:{tr_sents[:5]}, tr_labels: {tr_labels[:5]}')
+        if isDebug: print(f'ts_sents:{ts_sents[:5]}')
+        if isDebug: print(f'tr_tokens: {tr_tokens[:5]}\nts_tokens:{ts_tokens[:5]}')
+
+        # padding & truncation
+        max_length = 20
+        tr_tokens, _ = pad_sequence(tr_tokens, max_length)
+        ts_tokens, _ = pad_sequence(ts_tokens, max_length)
+        if isDebug: print(f'padded_tr_tokens:{tr_tokens[:5]}\nts_tokens:{ts_tokens[:5]}')
+
+        with open('./dataset/classification/glove_word.json', "r") as f:
+            glove_word = json.load(f)
+        word_dict = {}
+        word_embedding_matrix = []
+        for i, k in enumerate(glove_word.keys()):
+            word_dict[k] = i
+            word_embedding_matrix.append(torch.tensor(glove_word[k], requires_grad=False))
+        word_embedding_matrix = torch.stack(word_embedding_matrix, dim=0)
+        if isDebug: print(word_embedding_matrix.shape)
+        if isDebug: print(word_embedding_matrix[0])
+
+        train_x = word2vec(tr_tokens, word_dict, word_embedding_matrix)
+        test_x = word2vec(ts_tokens, word_dict, word_embedding_matrix)
+        if isDebug: print(train_x.shape, test_x.shape)
+        train_y = torch.tensor(tr_labels)
+
+        num_classes = 6
+        net = model.RNN(args, num_classes).to(device)
+        best_dev_acc = [-1]
+        best_weights = [None]
+
+        if args.do_train:
+            for epoch in range(args.epochs):
+                tr_loss, tr_acc = classification_train(train_x, train_y, epoch)
+                ts_loss, ts_acc = classification_test(train_x, train_y)
+                print("loss: {:.4f}, acc: {:.4f} ts_loss: {:.4f}, ts_acc: {:.4f}".format(tr_loss, tr_acc, ts_loss, ts_acc))
+                if ts_acc > best_dev_acc[0]:
+                    if hasattr(net, "module"):
+                        best_weights[0] = {k: v.to("cpu").clone() for k, v in net.module.state_dict().items()}
+                    else:
+                        best_weights[0] = {k: v.to("cpu").clone() for k, v in net.state_dict().items()}
+                    best_dev_acc[0] = ts_acc
+            torch.save(best_weights[0], "pytorch_classification_model.bin")
+        else:
+            print('do evaluation')
+            net.load_state_dict(torch.load('pytorch_classification_model.bin', map_location="cpu"))
+            net.to(device)
+            pred_y = classification_test(test_x)
+            output2csv(pred_y)
+
+    # pos-tagging
+    elif args.task == 'pos':
+        # load data
+        with open('./dataset/pos/train_set.json', 'r') as f:
+            train_data_dict = json.load(f)
+        with open('./dataset/pos/test_set.json', 'r') as f:
+            test_data_dict = json.load(f)
+        with open('./dataset/pos/tgt.txt', 'r') as f:
+            tgt = f.read()
+        tgt = tgt.split()
+        tgt_dict = {v: t for (t, v) in enumerate(tgt)}
+        if isDebug: print(f'tgt_dict: {tgt_dict}')
+        train_data = []
+        train_label = []
+        test_data = []
+        for k in train_data_dict.keys():
+            train_data.append(train_data_dict[k]['tokens'])
+            train_label.append(train_data_dict[k]['ud_tags'])
+
+        for k in test_data_dict.keys():
+            test_data.append(test_data_dict[k]['tokens'])
+
+        if isDebug: print(train_data[0], train_label[0])
+        train_max_length = 20
+
+        reverse_train_data = flip_input(train_data, train_max_length)
+        tr_x_tokens, tr_x_pad_start_index = pad_sequence(train_data, train_max_length, False)
+        reverse_tr_x_tokens, _ = pad_sequence(reverse_train_data, train_max_length, False)
+        tr_y_tokens, _ = pad_sequence(train_label, train_max_length, False)
+
+        test_max_length = max(len(x) for x in test_data)
+        if isDebug: print(f'test_max_length: {test_max_length}')
+        reverse_test_data = flip_input(test_data, test_max_length)
+        ts_x_tokens, ts_x_pad_start_index = pad_sequence(test_data, test_max_length, False)
+        reverse_ts_x_tokens, _ = pad_sequence(reverse_test_data, test_max_length, False)
+
+        if isDebug: print(f'tr_x_tokens[0]: {tr_x_tokens[0]}, reverse_tr_x_tokens[0]: {reverse_tr_x_tokens[0]}')
+        if isDebug: print(f'ts_x_tokens[0]: {ts_x_tokens[0]}, reverse_ts_x_tokens[0]: {reverse_ts_x_tokens[0]}')
+        with open('./dataset/pos/fasttext_word.json', "r") as f:
+            fasttext_word = json.load(f)
+        word_dict = {}
+        word_embedding_matrix = []
+        for i, k in enumerate(fasttext_word.keys()):
+            word_dict[k] = i
+            word_embedding_matrix.append(torch.tensor(fasttext_word[k], requires_grad=False))
+        word_embedding_matrix = torch.stack(word_embedding_matrix, dim=0)
+        if isDebug: print(word_embedding_matrix.shape)
+        if isDebug: print(word_embedding_matrix[0])
+        train_x = word2vec(tr_x_tokens, word_dict, word_embedding_matrix)
+        reverse_train_x = word2vec(reverse_tr_x_tokens, word_dict, word_embedding_matrix)
+
+        train_y = []
+        for label in tr_y_tokens:
+            words = []
+            for w in label:
+                words.append(tgt_dict[w])
+            train_y.append(words)
+        train_y = torch.tensor(train_y)
+
+        test_x = word2vec(ts_x_tokens, word_dict, word_embedding_matrix)
+        reverse_test_x = word2vec(reverse_ts_x_tokens, word_dict, word_embedding_matrix)
+        if isDebug: print(f'train_x.shape: {train_x.shape}, train_y.shape: {train_y.shape}, test_x.shape: {test_x.shape}')
+        if isDebug: print(f'train_x[0]: {train_x[0]}, train_y[0]: {train_y[0]}')
+
+        num_classes = 18
+        net = model.BidirectionalRNN(args, num_classes).to(device)
+        best_dev_acc = [-1]
+        best_weights = [None]
+
+        if args.do_train:
+            for epoch in range(args.epochs):
+                tr_loss, tr_acc = pos_train(train_x, reverse_train_x, tr_x_pad_start_index, train_max_length, train_y, epoch)
+                ts_loss, ts_acc = pos_test(train_x, reverse_train_x, tr_x_pad_start_index, train_max_length, train_y)
+                print("loss: {:.4f}, acc: {:.4f} ts_loss: {:.4f}, ts_acc: {:.4f}".format(tr_loss, tr_acc, ts_loss, ts_acc))
+                if ts_acc > best_dev_acc[0]:
+                    if hasattr(net, "module"):
+                        best_weights[0] = {k: v.to("cpu").clone() for k, v in net.module.state_dict().items()}
+                    else:
+                        best_weights[0] = {k: v.to("cpu").clone() for k, v in net.state_dict().items()}
+                    best_dev_acc[0] = ts_acc
+            torch.save(best_weights[0], "pytorch_pos_masked_model.bin")
+        else:
+            print('do evaluation')
+            net.load_state_dict(torch.load('pytorch_pos_model.bin', map_location="cpu"))
+            net.to(device)
+            print(test_x.shape, reverse_test_x.shape, len(ts_x_pad_start_index))
+            pred_y = pos_test(test_x, reverse_test_x, ts_x_pad_start_index, test_max_length)
+            output2csv(pred_y, 'pos_class.pred.csv')
---
+++ model.py
@@ -0,0 +1,158 @@
+import torch
+import torch.nn as nn
+import math
+
+class RNN(nn.Module):
+    def __init__(self, args, num_classes):
+        super(RNN, self).__init__()
+        self.device = torch.device(f'cuda:{args.gpu_num}')
+        self.embedding_layer = nn.Linear(300, 512)
+        self.num_layers = 3
+
+        self.hidden_size = 512
+
+        self.wx0 = self.make_layer(self.hidden_size, self.hidden_size)
+        self.wh0 = self.make_layer(self.hidden_size, self.hidden_size)
+        self.b0 = nn.Parameter(torch.zeros(self.hidden_size))
+
+        self.wx1 = self.make_layer(self.hidden_size, self.hidden_size)
+        self.wh1 = self.make_layer(self.hidden_size, self.hidden_size)
+        self.b1 = nn.Parameter(torch.zeros(self.hidden_size))
+
+        self.wx2 = self.make_layer(self.hidden_size, self.hidden_size)
+        self.wh2 = self.make_layer(self.hidden_size, self.hidden_size)
+        self.b2 = nn.Parameter(torch.zeros(self.hidden_size))
+
+
+        self.tanh = nn.Tanh()
+
+        self.fc = nn.Sequential(
+            nn.Linear(self.hidden_size, num_classes)
+        )
+    def forward(self, x):
+        x = self.embedding_layer(x)
+        h_t_0 = torch.zeros(x.size()[0], self.hidden_size).to(self.device)
+        h_t_1 = torch.zeros(x.size()[0], self.hidden_size).to(self.device)
+        h_t_2 = torch.zeros(x.size()[0], self.hidden_size).to(self.device)
+        for i in range(20):
+            v_t = x[:,i,:]
+            v_t = torch.squeeze(v_t, 1)
+            h_t_0 = self.tanh(torch.matmul(v_t, self.wx0) + torch.matmul(h_t_0, self.wh0) + self.b0)
+            h_t_1 = self.tanh(torch.matmul(h_t_0, self.wx1) + torch.matmul(h_t_1, self.wh1) + self.b1)
+            h_t_2 = self.tanh(torch.matmul(h_t_1, self.wx2) + torch.matmul(h_t_2, self.wh2) + self.b2)
+
+        logit = self.fc(h_t_2)
+        return logit
+
+    def make_layer(self, in_features, out_features):
+        linear = nn.Parameter(torch.Tensor(in_features, out_features))
+        nn.init.xavier_uniform_(linear)
+        return linear
+
+
+class BidirectionalRNN(nn.Module):
+    def __init__(self, args, num_classes):
+        super(BidirectionalRNN, self).__init__()
+        self.device = torch.device(f'cuda:{args.gpu_num}')
+        self.embedding_layer = nn.Linear(300, 512)
+        self.num_layers = 3
+        self.hidden_size = 512
+
+        self.wx0_l2r = self.make_layer(self.hidden_size, self.hidden_size)
+        self.wh0_l2r = self.make_layer(self.hidden_size, self.hidden_size)
+        self.b0_l2r = nn.Parameter(torch.zeros(self.hidden_size))
+
+        self.wx1_l2r = self.make_layer(self.hidden_size, self.hidden_size)
+        self.wh1_l2r = self.make_layer(self.hidden_size, self.hidden_size)
+        self.b1_l2r = nn.Parameter(torch.zeros(self.hidden_size))
+
+        self.wx2_l2r = self.make_layer(self.hidden_size, self.hidden_size)
+        self.wh2_l2r = self.make_layer(self.hidden_size, self.hidden_size)
+        self.b2_l2r = nn.Parameter(torch.zeros(self.hidden_size))
+
+
+        self.wx0_r2l = self.make_layer(self.hidden_size, self.hidden_size)
+        self.wh0_r2l = self.make_layer(self.hidden_size, self.hidden_size)
+        self.b0_r2l = nn.Parameter(torch.zeros(self.hidden_size))
+
+        self.wx1_r2l = self.make_layer(self.hidden_size, self.hidden_size)
+        self.wh1_r2l = self.make_layer(self.hidden_size, self.hidden_size)
+        self.b1_r2l = nn.Parameter(torch.zeros(self.hidden_size))
+
+        self.wx2_r2l = self.make_layer(self.hidden_size, self.hidden_size)
+        self.wh2_r2l = self.make_layer(self.hidden_size, self.hidden_size)
+        self.b2_r2l = nn.Parameter(torch.zeros(self.hidden_size))
+
+        self.tanh = nn.Tanh()
+        self.fc = nn.Sequential(
+            nn.Linear(self.hidden_size * 2, num_classes)
+        )
+
+    def forward(self, x, reverse_x, pad_start_index, max_length):
+        x_l2r = self.embedding_layer(x)
+        x_r2l = self.embedding_layer(reverse_x)
+        h_t_0_l2r = torch.zeros(x.size()[0], self.hidden_size).to(self.device)
+        h_t_1_l2r = torch.zeros(x.size()[0], self.hidden_size).to(self.device)
+        h_t_2_l2r = torch.zeros(x.size()[0], self.hidden_size).to(self.device)
+
+        h_t_0_r2l = torch.zeros(x.size()[0], self.hidden_size).to(self.device)
+        h_t_1_r2l = torch.zeros(x.size()[0], self.hidden_size).to(self.device)
+        h_t_2_r2l = torch.zeros(x.size()[0], self.hidden_size).to(self.device)
+
+        out_l2r = torch.tensor([])
+        out_r2l = torch.tensor([])
+
+        for i in range(max_length):
+            v_t_l2r = x_l2r[:,i,:]
+            v_t_l2r = torch.squeeze(v_t_l2r, 1)
+            h_t_0_l2r = self.tanh(torch.matmul(v_t_l2r, self.wx0_l2r) + torch.matmul(h_t_0_l2r, self.wh0_l2r) + self.b0_l2r)
+            h_t_1_l2r = self.tanh(torch.matmul(h_t_0_l2r, self.wx1_l2r) + torch.matmul(h_t_1_l2r, self.wh1_l2r) + self.b1_l2r)
+            h_t_2_l2r = self.tanh(torch.matmul(h_t_1_l2r, self.wx2_l2r) + torch.matmul(h_t_2_l2r, self.wh2_l2r) + self.b2_l2r)
+            unsqueeze_h_t_2_l2r = torch.unsqueeze(h_t_2_l2r, 1)
+            if len(out_l2r) == 0:
+                out_l2r = unsqueeze_h_t_2_l2r
+            else:
+                out_l2r = torch.cat([out_l2r, unsqueeze_h_t_2_l2r], dim=1)
+
+        for i in range(max_length):
+            v_t_r2l = x_r2l[:, i, :]
+            v_t_r2l = torch.squeeze(v_t_r2l, 1)
+            h_t_0_r2l = self.tanh(
+                torch.matmul(v_t_r2l, self.wx0_r2l) + torch.matmul(h_t_0_r2l, self.wh0_r2l) + self.b0_r2l)
+            h_t_1_r2l = self.tanh(
+                torch.matmul(h_t_0_r2l, self.wx1_r2l) + torch.matmul(h_t_1_r2l, self.wh1_r2l) + self.b1_r2l)
+            h_t_2_r2l = self.tanh(
+                torch.matmul(h_t_1_r2l, self.wx2_r2l) + torch.matmul(h_t_2_r2l, self.wh2_r2l) + self.b2_r2l)
+            unsqueeze_h_t_2_r2l = torch.unsqueeze(h_t_2_r2l, 1)
+            if len(out_r2l) == 0:
+                out_r2l = unsqueeze_h_t_2_r2l
+            else:
+                out_r2l = torch.cat([out_r2l, unsqueeze_h_t_2_r2l], dim=1)
+        concat_vector = self.ordered_concat(out_l2r, out_r2l, pad_start_index)
+        logit = self.fc(concat_vector)
+        logit = logit.view(-1, logit.size()[-1])
+        return logit
+
+
+    def make_layer(self, in_features, out_features):
+        linear = nn.Parameter(torch.Tensor(in_features, out_features))
+        nn.init.xavier_uniform_(linear)
+        return linear
+
+    def ordered_concat(self, l2r, r2l, pad_start_index):
+        concat_vectors = []
+        for i in range(len(l2r)):
+            concat_vector = []
+            p = pad_start_index[i]
+            for j in range(0, p):
+                v = torch.cat((l2r[i][j], r2l[i][p-j-1]), dim=-1)
+                concat_vector.append(v)
+            for j in range(p, len(l2r[0])):
+                v = torch.cat((l2r[i][j], r2l[i][j]), dim=-1)
+                concat_vector.append(v)
+            concat_vector = torch.stack(concat_vector, dim=0)
+            concat_vectors.append(concat_vector)
+        concat_vectors = torch.stack(concat_vectors, dim=0)
+
+        return concat_vectors
+
